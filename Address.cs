12d9fe0c3e8f4d9cad2706ae6dbdec4f841eab62
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-14 16:16:17 +0530

    Adding uc1

---
+++ Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook
+    {
+        ArrayList addresses=new ArrayList();
+        public void SetAddress()
+        {
+            Console.WriteLine("Enter City, state, zip");
+            addresses.Add(Console.ReadLine());
+        }
+    }
+}
